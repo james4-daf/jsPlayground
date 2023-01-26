0e9ff14bf8e6e6795b83841ce9ecd3b97527ca59
james4-daf <d.james@elsevier.com>
2023-01-26 10:20:21 +0000

    1st commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Playground for code snippets
+
+## How to run locally
+
+```
+node filename.js
+  e.g.
+node TernaryOperator.js
+```
+
+This will run in your terminal
---
+++ src/TernaryOperator.js
@@ -0,0 +1,10 @@
+function oldEnough(age) {
+  if (age < 18) {
+    return "Diet coke for you";
+  } else {
+    return "You are old to drink alcohol";
+  }
+}
+
+console.log(oldEnough(17));
+//Diet coke for you
